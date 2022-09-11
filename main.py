2002403bb32a6ef40dc782ecc37d0c4d602db55d
Hariwo <95012964+Hariwo711@users.noreply.github.com>
2022-09-11 15:02:54 +0700

    New

--- main.py
+++ main.py
@@ -5,32 +5,33 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 
 
+# kv = Builder.load_file("screen.kv")
 kv = Builder.load_file("screen.kv")
-
+kv2 = Builder.load_file("football.kv")
 
 class ScreenOne(Screen):
     pass
   
-class ScreenTwo(Screen):
-    pass
+# class ScreenTwo(Screen):
+#     pass
  
-class ScreenThree(Screen):
-    pass
+# class ScreenThree(Screen):
+#     pass
  
-class ScreenFour(Screen):
-    pass
+# class ScreenFour(Screen):
+#     pass
  
-class ScreenFive(Screen):
-    pass
+# class ScreenFive(Screen):
+#     pass
 
 
 
 screen_manager = ScreenManager()
 screen_manager.add_widget(ScreenOne(name ="screen_one"))
-screen_manager.add_widget(ScreenTwo(name ="screen_two"))
-screen_manager.add_widget(ScreenThree(name ="screen_three"))
-screen_manager.add_widget(ScreenFour(name ="screen_four"))
-screen_manager.add_widget(ScreenFive(name ="screen_five"))
+# screen_manager.add_widget(ScreenTwo(name ="screen_two"))
+# screen_manager.add_widget(ScreenThree(name ="screen_three"))
+# screen_manager.add_widget(ScreenFour(name ="screen_four"))
+# screen_manager.add_widget(ScreenFive(name ="screen_five"))
 
 
 # Create the App class
